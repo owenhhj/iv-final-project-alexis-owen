d2cb283f3c425f08d5482315eaeb86c42c668c67
h <juanjuanjks@163.com>
2022-04-17 17:07:00 +0800

    Tooltip.js: unable to fetch img error 403

--- src/owen/Tooltip.js
+++ src/owen/Tooltip.js
@@ -1,23 +1,58 @@
 import './Tooltip.css';
+import {csv} from 'd3';
+import {useEffect, useState} from "react";
 
-
-const fakeSchools = [
-  {}
-];
+function useData(csvPath, row=3) {
+  const [dataAll, setData] = useState(null);
+  useEffect(() => {
+    csv(csvPath).then(data => {
+      setData(data.slice(0, row));
+    });
+  }, []);
+  return dataAll;
+}
 
 export default function Tooltip({
   title='Default Tooltip Title',
   city='Shanghai',
-  schoolsData={}
+  schoolsData=[]
                                 }) {
-
+  let data = useData('./data.csv')
 
   return (
-    <div className={'Tooltip'}>
-      <p>{title}</p>
+    <div className={'Tooltip card non-text'}>
+      <div className={'tooltip-row'}>
+        <p>{title}</p>
+      </div>
+      <div className={'tooltip-row'}>
+        <p>{city}</p>
+      </div>
+
+      {data && data.map((uni, index) => {
+        return (
+          <TooltipUniEntry key={index} uni={uni}/>
+        );
+      })}
+
     </div>
   );
 };
 
+function TooltipUniEntry({
+  uni={}
+                          }) {
+  console.log(uni.link, uni.logo)
 
+  return (
+    <div className={'TooltipUniEntry'}>
+      <div className={'logo-and-name'}>
+        {/*<img src={uni.logo} alt=""/>*/}
+        <p>{uni.university}</p>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
+}
 
